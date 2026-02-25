6c906c63415852f67666aafbbf7cf101845d2e28
First-Demo <shivamgohel2102@gmail.com>
2026-02-25 18:30:55 +0530

    navbar changing

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('browse/', views.browse, name='browse'),
+    path('collections/', views.collections, name='collections'),
+    path('how-it-works/', views.how_it_works, name='how_it_works'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-
 # Create your views here.
+
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
+
+def browse(request):
+    return render(request, 'browse.html')
+
+def collections(request):
+    return render(request, 'collections.html')
+
+def how_it_works(request):
+    return render(request, 'how_it_works.html')
